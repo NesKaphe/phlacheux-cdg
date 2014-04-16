88ba1ae21ab68a0e2541b83e8d4f9a818ad46b62
GALICHET DAVID <galichet.david@gmail.com>
2014-04-16 16:07:44 +0200

    peut rendre invisible les menusaad object, add 8 button

--- src/Visionneuse.java
+++ src/Visionneuse.java
@@ -3,15 +3,20 @@ import java.awt.event.*;
 import javax.swing.*;
 
 import affichage.Toile;
+import formes.*;
 
 
 public class Visionneuse implements Runnable {
 	
 	private Toile toile;
+	private ObjetGeometrique selection;
 	private JMenuBar menuBar;
 	private JToolBar menu_object_add;
 	private JToolBar menu_manage_ani;
-	private JToolBar menu_3;
+	private JToolBar menu_manage_object;
+	
+	private JColorChooser choicecolor;
+	private JDialog dialog;
 	
     public void run() {
     	JFrame frame = new JFrame("Visionneuse");
@@ -22,44 +27,93 @@ public class Visionneuse implements Runnable {
 		// on crée un menuBar static avec juste un bouton pour quitter et recharger la page
 		menuBar = new JMenuBar();
 		JMenu menu = new JMenu("Fichier");
-		JMenuItem mi1 = new JMenuItem("Recharger");
-    	JMenuItem mi2 = new JMenuItem("Quitter");
-    	menu.add(mi1);
-    	menu.add(mi2);
+		JMenuItem mi_new = new JMenuItem("Nouvelle Toile");
+		JMenuItem mi_refresh = new JMenuItem("Rafraichir");
+		JMenuItem mi_add_object = new JMenuItem("Ajouter un forme");
+    	JMenuItem mi_exit = new JMenuItem("Quitter");
+    	menu.add(mi_new);
+    	menu.add(mi_refresh);
+    	menu.add(mi_add_object);
+    	menu.add(mi_exit);
     	menuBar.add(menu);
     	
     	//Les menu pouvant se déplacer
     	Container c = frame.getContentPane();
     	c.setLayout(new BorderLayout());
+    	
+    	// menu d'ajout d'objet
     	menu_object_add = new JToolBar(SwingConstants.HORIZONTAL);
     	menu_object_add.setFloatable(true);
     	menu_object_add.setRollover(true);
+    	menu_object_add.setName("Add object");
+    	JPanel list_form= new JPanel();
     	// Bouton objectGeometrique Cercle
     	JButton cercle = new JButton("Cercle");
 	    cercle.setToolTipText("Aide pour "+cercle.getText());
-	    menu_object_add.add(cercle);
+	    list_form.add(cercle);
 	    // Bouton objectGeometrique Cercle
     	JButton triangle = new JButton("Triangle");
     	triangle.setToolTipText("Aide pour "+triangle.getText());
-	    menu_object_add.add(triangle);
+    	list_form.add(triangle);
 	    // Bouton objectGeometrique Cercle
     	JButton rectangle = new JButton("Rectangle");
     	rectangle.setToolTipText("Aide pour "+rectangle.getText());
-	    menu_object_add.add(rectangle);
+    	list_form.add(rectangle);
+	    JButton ligne = new JButton("Ligne");
+    	ligne.setToolTipText("Aide pour "+ligne.getText());
+    	list_form.add(ligne);
+    	// Bouton objectGeometrique Cercle
+    	JButton b4 = new JButton("b4");
+	    list_form.add(b4);
+	    // Bouton objectGeometrique Cercle
+    	JButton b5= new JButton("b5");
+    	list_form.add(b5);
+	    // Bouton objectGeometrique Cercle
+    	JButton b6 = new JButton("b6");
+    	list_form.add(b6);
+	    JButton b7 = new JButton("b7");
+    	list_form.add(b7);
+    	menu_object_add.add(list_form);
+	    
+	    // menu information ObjetGeometrique selection
+	    menu_manage_object = new JToolBar(SwingConstants.HORIZONTAL);
+	    menu_manage_object.setFloatable(true);
+	    menu_manage_object.setRollover(true);
+	    menu_manage_object.setName("Manage Object");
+    	menu_manage_object.setVisible(false);
+    	JButton l = new JButton("Ligne");
+    	ligne.setToolTipText("Aide pour "+ligne.getText());
+    	menu_manage_object.add(l);
+	    
 	    c.add(menu_object_add,BorderLayout.SOUTH);
+	    c.add(menu_manage_object,BorderLayout.NORTH);
 	    c.add(toile,BorderLayout.CENTER);
-	   
-	    
-	    
 
-    	mi1.addActionListener(new ActionListener() {
+    	mi_new.addActionListener(new ActionListener() {
     		public void actionPerformed(ActionEvent e) {
     			//TODO effectuer une recharge de la Toile
     		}
     	});
     	
+    	mi_refresh.addActionListener(new ActionListener() {
+    		public void actionPerformed(ActionEvent e) {
+    			//TODO effectuer une recharge de la Toile
+    		}
+    	});
+    	
+    	mi_add_object.addActionListener(new ActionListener() {
+    		public void actionPerformed(ActionEvent e) {
+    			if (menu_object_add.isVisible()){
+    				menu_object_add.setVisible(false);
+    			}
+    			else {
+    				menu_object_add.setVisible(true);
+    			}
+    		}
+    	});
+    	
     	// le champ quitter du menu ferme tout
-    	mi2.addActionListener(new ActionListener() {
+    	mi_exit.addActionListener(new ActionListener() {
     		public void actionPerformed(ActionEvent e) {
     			System.exit(0);
     		}
@@ -78,6 +132,12 @@ public class Visionneuse implements Runnable {
     	});
     	
     	rectangle.addActionListener(new ActionListener() {
+    		public void actionPerformed(ActionEvent e) {
+    			//TODO choisir Structure rectangle et Menu Création Spécification de l'objet
+    		}
+    	});
+    	
+    	ligne.addActionListener(new ActionListener() {
     		public void actionPerformed(ActionEvent e) {
     			//TODO choisir Structure rectangle et Menu Création Spécification de l'objet
     		}
