529ebb768d19c951231596283fa7ca9e927d3967
dias <alain.dias02@gmail.com>
2014-04-24 14:45:26 +0200

    passage de visionneur en variable de classe

--- src/listeners/CreateAnimationListener.java
+++ src/listeners/CreateAnimationListener.java
@@ -1,5 +1,6 @@
 package listeners;
 
+import java.awt.BasicStroke;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
@@ -12,36 +13,43 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import formes.ObjetGeometrique;
 import formes.SegmentDroite;
 
+import Animations.Animation;
 import Animations.Comportement;
 import Animations.CompositeAnimation;
+import Animations.Rotation;
+import Animations.StrokeWidth;
 import affichage.Item;
+import affichage.VisionneuseAnimation;
 
 /**
  * Listener de creation d'animations
  * @author Alain
  *
  */
-public class CreateAnimationListener implements ActionListener{
+public class CreateAnimationListener implements ActionListener {
 
 	JList<Item> liste;
+	VisionneuseAnimation visionneuse;
 
 	JComboBox<String> comboBoxAnimations;
 	
-	//Frame pour l'alertBox
-	JFrame alertFrame;
 	
 	//Champs
 	JPanel champsDynamiques;
 	JTextField angle;
 	JTextField epaisseur;
+	JTextField t_debut;
+	JTextField t_fin;
 	
 	JOptionPane optionPane;
 	
-	public CreateAnimationListener(JList<Item> liste) {
+	public CreateAnimationListener(JList<Item> liste, VisionneuseAnimation visionneuse) {
 		this.liste = liste;
 		this.comboBoxAnimations = null;
+		this.visionneuse = visionneuse;
 	}
 	
 	
@@ -61,11 +69,13 @@ public class CreateAnimationListener implements ActionListener{
 			//On affiche maintenant notre OptionBox
 			Object[] message = {"Animation :", this.comboBoxAnimations};
 			
-			int reponseSelection = JOptionPane.showConfirmDialog(null, message, "Création d'animation", JOptionPane.OK_CANCEL_OPTION);
+			int reponseSelection = JOptionPane.showConfirmDialog(null, message, "Création ["+comp.toString()+"]", JOptionPane.OK_CANCEL_OPTION);
 			if(reponseSelection == JOptionPane.OK_OPTION) {
 				String selection = this.comboBoxAnimations.getSelectedItem().toString();
 				int reponseAnim = this.afficheAlertBoxOf(selection);
-				
+				if(reponseAnim == JOptionPane.OK_OPTION) {
+					this.creerAnimation(comp, selection);
+				}
 			}
 			break;
 		case "modification": //Pas sur mais je prefere le prevoir
@@ -102,10 +112,14 @@ public class CreateAnimationListener implements ActionListener{
 			messageFinal[i] = message[i];
 		}
 		
+		//Temps debut et fin
+		this.t_debut = new JTextField("0");
+		this.t_fin = new JTextField("0");
+		
 		messageFinal[messageFinal.length-4] = "Temps debut";
-		messageFinal[messageFinal.length-3] = new JTextField("0");
+		messageFinal[messageFinal.length-3] = this.t_debut;
 		messageFinal[messageFinal.length-2] = "Temps fin";
-		messageFinal[messageFinal.length-1] = new JTextField("0");
+		messageFinal[messageFinal.length-1] = this.t_fin;
 		return JOptionPane.showConfirmDialog(null, messageFinal, "Paramètres "+selection, JOptionPane.OK_CANCEL_OPTION);
 	}
 
@@ -142,8 +156,40 @@ public class CreateAnimationListener implements ActionListener{
 		//A implementer plus tard
 	}
 	
+	//TODO: peut etre le rendre public ou non pour la translation (=non si on peut envoyer un action command depuis le clavier)
 	private void creerAnimation(Comportement comp, String selection) {
+		Animation anim = null;
+		double tempsDebut = Double.parseDouble(this.t_debut.getText());
+		double tempsFin = Double.parseDouble(this.t_fin.getText());
 		
+		switch(selection) {
+		case "Rotation":
+			double angleRad = Math.toRadians(Double.parseDouble(this.angle.getText()));
+			anim = new Rotation(tempsDebut,tempsFin, 0, angleRad, comp.getObjGeo().getCentre());
+			break;
+		case "Translation":
+			//TODO: je sais pas comment le faire
+			break;
+		case "Epaisseur de trait":
+			//On va calculer la difference entre l'epaisseur demandée et l'epaisseur de l'objet
+			ObjetGeometrique obj = comp.getEtatObjGeo(0.); //TODO: recup le temps courant de l'edition
+			float epaisseurObj = obj.getStroke().getLineWidth();
+			float epaisseurDemande = Float.parseFloat(this.epaisseur.getText());
+			anim = new StrokeWidth(tempsDebut, tempsFin, 0, epaisseurDemande - epaisseurObj);
+			break;
+		case "Couleur de trait":
+			//TODO: JColorchooser
+			break;
+		case "Couleur de fond":
+			break;
+		}
+		
+		//On ajoute maintenant l'animation au comportement
+		CompositeAnimation ca = (CompositeAnimation) comp.getAnimation();
+		ca.add(anim);
+		
+		//Et on met a jour le dessin de notre visionneuse d'animation
+		this.visionneuse.dessineAnimation();
 	}
 	
 	/*
